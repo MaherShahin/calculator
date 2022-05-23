24c9ff7a00ec6c496c934e05fdbf18f49962e2e9
MaherShahin <abdomaher2010@hotmail.com>
2022-05-23 13:39:04 +0200

    can solve two operands now!

--- app.js
+++ app.js
@@ -35,6 +35,36 @@ function operate(operator,a,b){
 const container = document.querySelector('.container');
 const screen = document.querySelector('.screen');
 
+let screenText = document.querySelector('.screen').innerText
+
+// add 5 buttons for +,-,*,/,=
+const plusBtn = document.createElement('div');
+plusBtn.className = `plusBtn oper`;
+plusBtn.innerText = '+';
+container.appendChild(plusBtn)
+
+const minusBtn = document.createElement('div');
+minusBtn.className = `minusBtn oper`;
+minusBtn.innerText = '-';
+container.appendChild(minusBtn)
+
+const timesBtn = document.createElement('div');
+timesBtn.className = `timesBtn oper`;
+timesBtn.innerText = '*';
+container.appendChild( timesBtn);
+
+const divideBtn = document.createElement('div');
+divideBtn.className = `divideBtn oper`;
+divideBtn.innerText = '/';
+container.appendChild(divideBtn);
+
+//
+const equalBtn = document.createElement('div');
+equalBtn.className = `equalBtn oper`;
+equalBtn.innerText = '=';
+container.appendChild(equalBtn)
+
+//adding all the numbers on container
 function addNumbers(){
     for (i=9; i>=0; i--){
         const numberBtn = document.createElement('div');
@@ -52,4 +82,54 @@ for (i=0;i<numBtnsNodeList.length;i++){
     item.addEventListener('click', () => {
         screen.innerText = screen.innerText + item.innerText;
     })
+}
+
+//functionality for writing the operators on the screen when pressing their btn and making sure not more than 1 operator is written in sequence
+let operBtnsNodeList = document.querySelectorAll('.oper');
+//operator buttons without =
+for (i=0;i<operBtnsNodeList.length-1;i++){
+    let item = operBtnsNodeList[i];
+    item.addEventListener('click', () => {
+        checkLastOper();
+        screen.innerText = screen.innerText + item.innerText;
+    })
+}
+// operator = 
+let operateBtn = operBtnsNodeList[operBtnsNodeList.length-1];
+operateBtn.addEventListener('click', () => {
+    solveTwoOperands();  
+})
+
+// Clear Btn
+const clearBtn = document.createElement('div');
+clearBtn.className = 'clearBtn';
+clearBtn.innerText = 'Clear';
+clearBtn.addEventListener('click', ()=> {
+    screen.innerText = '';
+})
+container.appendChild(clearBtn)
+
+//Checks if last input on the screen is an operator
+
+function checkLastOper () {
+    if (screen.innerText[screen.innerText.length-1] === '+' || screen.innerText[screen.innerText.length-1] === '-' || screen.innerText[screen.innerText.length-1] === '*' || screen.innerText[screen.innerText.length-1] === '/' || screen.innerText[screen.innerText.length-1] === '='){
+        screen.innerText[screen.innerText.length-1] = item.innerText;
+        return;
+    }
+}
+
+function solveTwoOperands(){
+if(screen.innerText.includes('+')){
+    let operands = screen.innerText.split('+')
+    screen.innerText = operate('+',parseInt(operands[0]),parseInt(operands[1]))
+} else if(screen.innerText.includes('-')){
+    let operands = screen.innerText.split('-')
+    screen.innerText = operate('-',parseInt(operands[0]),parseInt(operands[1]))
+} else if(screen.innerText.includes('/')){
+    let operands = screen.innerText.split('/')
+    screen.innerText = operate('/',parseInt(operands[0]),parseInt(operands[1]))
+} else if(screen.innerText.includes('*')){
+    let operands = screen.innerText.split('*')
+    screen.innerText = operate('*',parseInt(operands[0]),parseInt(operands[1]))
+}     
 }
